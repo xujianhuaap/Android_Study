5fcaa7d3f0d2608018755447db50fd825a00ad57
skullmind <xujianhuaap@gmail.com>
2018-12-15 19:34:15 +0800

    ssss

--- app/src/main/java/com/example/xujianhua/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xujianhua/myapplication/MainActivity.java
@@ -1,52 +1,18 @@
 package com.example.xujianhua.myapplication;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.res.Configuration;
-import android.os.IBinder;
-import android.os.PersistableBundle;
-import android.os.RemoteException;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
-
-
-import com.example.app.BookOuterClass;
-import com.example.xujianhua.myapplication.service.PlayerService;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
     private MusicPlayer musicPlayer;
+    private boolean isShow = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(MainActivity.this,PlayerService.class);
-        bindService(intent, new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                musicPlayer = MusicPlayer.Stub.asInterface(service);
-                try {
-                    musicPlayer.getMusicPlayerName();
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
+        setContentView(R.layout.activity_booking_main);
 
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-
-            }
-        },BIND_AUTO_CREATE);
     }
 
 
@@ -103,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         Log.d(MainActivity.class.getName(),"onConfigurationChanged");
+
+    }
+
+    public void openAnimation(){
+
+    }
+
+    public void closeAnimation(){
 
     }
 }
